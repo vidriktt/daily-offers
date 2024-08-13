ccf3b940a3d71bd3517c65ca75ff5c9a6308a37f
Vidrik Toom Tabas <vidriktoom@gmail.com>
2024-08-13 17:21:00 +0300

    initial project structure for a Chrome plugin

---
+++ background.js
@@ -0,0 +1,36 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	(async () => {
+		const apiURL = '';
+
+		switch (request.action) {
+			case "getData":
+				try {
+					const response = await fetch(`${apiURL}`, {
+						method: 'GET',
+					});
+					const html = await response.text();
+					await sendResponse({
+						success: true,
+						payload: {
+							status: response.status,
+							html,
+							req: request.payload,
+						}
+					});
+				} catch (error) {
+					console.error(error);
+					await sendResponse({
+						success: false,
+						payload: {
+							error,
+							message: error.message,
+							req: request.payload,
+						}
+					});
+				}
+				break;
+		}
+	})();
+
+	return true;
+});
